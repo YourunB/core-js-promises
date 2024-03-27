49abf36b65be1cfc2bb92069f39f9a9759ec58b2
Yury Butskevich <bxyura@gmail.com>
2024-03-27 16:11:58 +0300

    feat: complete queuPromises func

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -139,8 +139,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let res = Promise.resolve('');
+  promises.forEach((promise) => {
+    res = res.then((newRes) => promise.then((v) => newRes + v));
+  });
+  return res;
 }
 
 module.exports = {
